14afc90136dee13279ea53fdbe070b1b08b3aafa
Jason Gardner <im@jasongardner.co>
2022-09-21 17:00:54 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/x/canvas@v1.4.1/mod.ts";
-export { decode } from "https://deno.land/std@0.156.0/encoding/base64.ts";
+export * from "https://deno.land/std@0.156.0/encoding/base64.ts";
